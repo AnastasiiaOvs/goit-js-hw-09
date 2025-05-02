4c5edb304e515288149e0177e38e99a86da9a38b
AnastasiiaOvs <a.alexandrovna@i.ua>
2025-05-03 01:23:26 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,30 +1,27 @@
 const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
 
-let formData = {
-  email: '',
-  message: '',
-};
-
 loadFormData();
 
 form.addEventListener('input', onInputChange);
 form.addEventListener('submit', onFormSubmit);
 
-function onInputChange(e) {
-  const { name, value } = e.target;
-  formData[name] = value.trim();
+function onInputChange() {
+  const formData = {
+    email: form.elements.email.value.trim(),
+    message: form.elements.message.value.trim(),
+  };
+
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function loadFormData() {
   const savedData = localStorage.getItem(STORAGE_KEY);
+
   if (!savedData) return;
 
   try {
     const parsedData = JSON.parse(savedData);
-
-    formData = { ...formData, ...parsedData };
 
     if (parsedData.email) form.elements.email.value = parsedData.email;
     if (parsedData.message) form.elements.message.value = parsedData.message;
@@ -36,23 +33,16 @@ function loadFormData() {
 function onFormSubmit(e) {
   e.preventDefault();
 
-  const { email, message } = formData;
+  const email = form.elements.email.value.trim();
+  const message = form.elements.message.value.trim();
 
   if (email === '' || message === '') {
     alert('Fill please all fields');
     return;
   }
 
-  console.log(formData);
+  console.log({ email, message });
 
   form.reset();
   localStorage.removeItem(STORAGE_KEY);
-  formData = { email: '', message: '' };
 }
-
-const textarea = document.querySelector('.feedback-form textarea');
-
-textarea.addEventListener('input', () => {
-  textarea.style.height = '80px';
-  textarea.style.height = textarea.scrollHeight + 'px'; 
-});
